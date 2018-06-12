0a7a1d8ca3c4e5ee136a184fd37488778176bf33
unknown <muka040789@hotmail.com>
2018-06-11 23:09:33 -0300

    add API PontoTuristico

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from core.models import PontoTuristico
+from .serializers import PontoTuristicoSerializer
 
 
-class AccountViewSet(ModelViewSet):
-    queryset = Account.objects.all()
-    serializer_class = AccountSerializer
+class PontoTuristicoViewSet(ModelViewSet):
+    queryset = PontoTuristico.objects.all()
+    serializer_class = PontoTuristicoSerializer
