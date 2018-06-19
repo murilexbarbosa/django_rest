02500b131fd36edd9e31a15df4b14aec542a0e90
Murilo Barbosa <muka040789@hotmail.com>
2018-06-19 12:31:54 -0300

    Add API avaliacoes e comentarios"

--- enderecos/api/serializers.py
+++ enderecos/api/serializers.py
@@ -5,5 +5,5 @@ from enderecos.models import Endereco
 class EnderecoSerializer(ModelSerializer):
     class Meta:
         model = Endereco
-        fields = ('linha1', 'linha2', 'cidade', 'estado', 'pais', 'latitude',
+        fields = ('id', 'linha1', 'linha2', 'cidade', 'estado', 'pais', 'latitude',
                   'longitude')
