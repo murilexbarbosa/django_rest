07d23089c6734cce1ebbab3e65ff0017a768cab1
Murilo Barbosa <muka040789@hotmail.com>
2018-07-12 16:29:44 -0300

    Add campo Observacoes para Atracoes

--- atracoes/models.py
+++ atracoes/models.py
@@ -6,6 +6,7 @@ class Atracao(models.Model):
     horario_func = models.TextField()
     idade_minima = models.IntegerField()
     foto = models.ImageField(upload_to='atracoes', blank=True, null=True)
+    observacoes = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.nome
