f346438141eb2ac66e9c8056b70bd053fb826db4
unknown <muka040789@hotmail.com>
2018-07-12 22:07:02 -0300

    add gravação ponto

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -15,6 +15,19 @@ class PontoTuristicoSerializer(ModelSerializer):
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes',
                   'comentarios', 'avaliacoes', 'endereco', 'descricao_completa',
                   'descricao_completa2')
+        read_only_fields = ('comentarios', 'avaliacoes')
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
