ed5a41e9ba63cda26991df6f0d833bd5d0f106d1
Murilo Barbosa <muka040789@hotmail.com>
2018-07-16 17:32:12 -0300

    Adiciona model docfiscal

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from core.models import DocIdentificacao
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from rest_framework.fields import SerializerMethodField
 from atracoes.models import Atracao
 from enderecos.models import Endereco
 
+class DocIdentificacaoSerializer(ModelSerializer):
+    class Meta:
+        model = DocIdentificacao
+        fields = '__all__'
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer(many=False)
     descricao_completa = SerializerMethodField()
+    doc_identificacao = DocIdentificacaoSerializer()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes',
                   'comentarios', 'avaliacoes', 'endereco', 'descricao_completa',
-                  'descricao_completa2')
+                  'descricao_completa2', 'doc_identificacao')
         read_only_fields = ('comentarios', 'avaliacoes')
 
     def cria_atracoes(self, atracoes, ponto):
@@ -33,6 +39,10 @@ class PontoTuristicoSerializer(ModelSerializer):
         endereco = validated_data['endereco']
         del validated_data['endereco']
 
+        ##carregando doc identificacao
+        doc = validated_data['doc_identificacao']
+        del validated_data['doc_identificacao']
+
         ##criando objeto ponto turistico
         ponto = PontoTuristico.objects.create(**validated_data)
 
@@ -42,6 +52,10 @@ class PontoTuristicoSerializer(ModelSerializer):
         ##preenchendo endereco ao objeto ponto turistico
         end = Endereco.objects.create(**endereco)
         ponto.endereco = end
+
+        ##preenchendo doc_identificacao
+        doc1 = DocIdentificacao.objects.create(**doc)
+        ponto.doc_identificacao = doc1
 
         ponto.save()
         ##retornando objeto para gravação
--- core/models.py
+++ core/models.py
@@ -4,6 +4,8 @@ from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
 from enderecos.models import Endereco
 
+class DocIdentificacao(models.Model):
+    description = models.CharField(max_length=100)
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
@@ -15,6 +17,8 @@ class PontoTuristico(models.Model):
     endereco = models.ForeignKey(
         Endereco, on_delete=models.CASCADE, null=True, blank=True)
     foto = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
+    doc_identificacao = models.OneToOneField(
+        DocIdentificacao, on_delete=models.CASCADE, null=True, blank=True)
 
     @property
     def descricao_completa2(self):
