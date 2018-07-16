b53c7e7004a2ce3304c7708817d99ba51fd22232
Murilo Barbosa <muka040789@hotmail.com>
2018-07-16 09:59:57 -0300

    incluindo registros many to many e foreing key

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -3,6 +3,8 @@ from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from rest_framework.fields import SerializerMethodField
+from atracoes.models import Atracao
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(ModelSerializer):
@@ -23,10 +25,27 @@ class PontoTuristicoSerializer(ModelSerializer):
             ponto.atracoes.add(at)
 
     def create(self, validated_data):
+        ##carregando atracoes
         atracoes = validated_data['atracoes']
         del validated_data['atracoes']
+
+        ##carregando endereco
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
+        ##criando objeto ponto turistico
         ponto = PontoTuristico.objects.create(**validated_data)
+
+        ##preenchendo atracoes ao objeto ponto turistico
         self.cria_atracoes(atracoes, ponto)
+
+        ##preenchendo endereco ao objeto ponto turistico
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+        ##retornando objeto para gravação
+        return ponto
 
 
     def get_descricao_completa(self, obj):
